88034f2fb41505fc444f5b14594a4599f5dcc0ae
omkar khare <omkar_khare@persistent.co.in>
2020-05-11 12:23:01 +0530

    commented db creation resource

--- terraws/rds.tf
+++ terraws/rds.tf
@@ -12,7 +12,7 @@ locals {
 
 
 resource "aws_secretsmanager_secret" "terrasecret" {
-  name = "terrasecret1"
+  name = "terrasecret"
 }
 
 resource "aws_secretsmanager_secret_version" "terrasecversion" {
@@ -20,24 +20,24 @@ resource "aws_secretsmanager_secret_version" "terrasecversion" {
   secret_string = "${random_string.terrastring.result}"
 }
 
-resource "aws_db_instance" "terradb" {
-    depends_on = [ aws_db_subnet_group.terrasubgr, aws_security_group.terrasg ]
-    identifier = "${local.db_identifier}"
-    allocated_storage = 20
-    storage_type = "gp2"
-    engine = "mysql"
-    engine_version = "8.0.15"
-    instance_class = "${var.db_instance_class}"
-    name = "terradb1"
-    username = "terrauser"
-    password = "${aws_secretsmanager_secret_version.terrasecversion.secret_string}"
-    parameter_group_name = "${var.pgname}"
-    max_allocated_storage = 100
-    apply_immediately = "true"
-    db_subnet_group_name = "${aws_db_subnet_group.terrasubgr.id}"
-    port = 3306
-    multi_az = "true"
-    vpc_security_group_ids = ["${aws_security_group.terrasg.id}"]
-    skip_final_snapshot = "true"
-    final_snapshot_identifier = "${local.db_snapshot}"
+#resource "aws_db_instance" "terradb" {
+ #   depends_on = [ aws_db_subnet_group.terrasubgr, aws_security_group.terrasg ]
+  #  identifier = "${local.db_identifier}"
+   # allocated_storage = 20
+    #storage_type = "gp2"
+    #engine = "mysql"
+    #engine_version = "8.0.15"
+    #instance_class = "${var.db_instance_class}"
+    #name = "terradb1"
+    #username = "terrauser"
+    #password = "${aws_secretsmanager_secret_version.terrasecversion.secret_string}"
+    #parameter_group_name = "${var.pgname}"
+    #max_allocated_storage = 100
+    #apply_immediately = "true"
+    #db_subnet_group_name = "${aws_db_subnet_group.terrasubgr.id}"
+    #port = 3306
+    #multi_az = "true"
+    #vpc_security_group_ids = ["${aws_security_group.terrasg.id}"]
+    #skip_final_snapshot = "true"
+    #final_snapshot_identifier = "${local.db_snapshot}"
 }
