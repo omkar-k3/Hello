64d1b174ac736ca1f66a1fe427878b585d091fa3
omkar-k3 <omkarkhare31@gmail.com>
2024-05-31 16:37:15 +0530

    react test

---
+++ frontend/ecommerce/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ecommerce</a>
+      </header>
+      <main>
+        list Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
